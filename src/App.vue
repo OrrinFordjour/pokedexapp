9efeabf3c152c410b63de4272ff19a13e9ae026a
orrinfordjour <orrinfordjour1990@gmail.com>
2022-12-12 14:34:14 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p class="underline">hier ben ik </p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PokeGrid from './components/PokeGrid.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PokeGrid
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
